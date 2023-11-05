93856469a318f2cbfc930a0ac138fd9090b80653
Vinay S <124019116+vinay-s36@users.noreply.github.com>
2023-11-05 10:19:58 +0530

    Delete backend directory

--- backend/backendapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BackendappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backendapp'
--- backend/backendapp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class image(models.Model):
-    name=models.IntegerField()
-    image1=models.ImageField(upload_to='images/')
